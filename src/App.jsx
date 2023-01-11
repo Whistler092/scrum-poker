5be12172a703a635914c34be73b87cb09e5d1dc8
Ramiro Bedoya <iamramiroo@gmail.com>
2023-01-10 21:16:53 -0500

    Test Env File

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+const BASE_URL = import.meta.env.VITE_APP_NAME;
+console.log(BASE_URL);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -28,7 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
