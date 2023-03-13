f8c1af198e6a03401bc818251ebe5abdda037084
Ramiro Andrés Bedoya E <iamramiroo@gmail.com>
2023-03-13 13:12:14 -0500

    Update icons

--- src/utils/cardsHelper.js
+++ src/utils/cardsHelper.js
@@ -5,10 +5,12 @@ export const CARDS = [
   { id: 3, key: 3, isActive: false },
   { id: 4, key: 5, isActive: false },
   { id: 5, key: 8, isActive: false },
-  { id: 6, key: 13, isActive: false },
-  { id: 7, key: 21, isActive: false },
-  { id: 9, key: "?", isActive: false },
-  { id: 10, key: "∞", isActive: false },
+  { id: 6, key: 8, isActive: false },
+  { id: 7, key: 11, isActive: false },
+  { id: 8, key: 13, isActive: false },
+  { id: 9, key: 21, isActive: false },
+  { id: 10, key: "?", isActive: false },
+  { id: 11, key: "∞", isActive: false },
 ];
 
 export const getRandomPattern = () => {
